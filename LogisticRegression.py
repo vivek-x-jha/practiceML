6c2b476fa0c6e310484ada5435a767e67248ca36
Vivek Jha <vivek.x.jha@gmail.com>
2018-03-31 03:45:37 +0530

    Added data setup in dataframes

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,3 +1,50 @@
 # Implement Logistic Regression from scratch
 
+# Performs Linear Regression (from scratch) using randomized data
+# Optimizes weights by using Gradient Descent Algorithm
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+
+features = 3
+trainingSize = 10 ** 1
+trainingSteps = 10 ** 3
+learningRate = 10 ** -2
+
+randFeatures = np.random.rand(trainingSize, features)
+flags = np.random.randint(2, size=(trainingSize, 1))
+randData = np.concatenate((randFeatures, flags), axis=1)
+colNames = [f'f{i}' for i in range(1, features + 1)]
+colNames.append('labels')
+
+dummy_column = pd.Series(np.ones(trainingSize), name='f0')
+df = pd.DataFrame(randData, columns=colNames)
+
+X = pd.concat([dummy_column, df.drop(columns='labels')], axis=1)
+y = df['labels']
+thetas = np.random.rand(features + 1)
+
+# cost = lambda thetas: np.mean((np.matmul(X, thetas) - y) ** 2) / 2
+# dJdtheta = lambda thetas, k: np.mean((np.matmul(X, thetas) - y) * X.iloc[:, k])
+# gradient = lambda thetas: np.array([dJdtheta(thetas, k) for k in range(X.shape[1])])
+
+# # J(theta) before gradient descent
+# print(cost(thetas))
+
+# # Perform gradient descent
+# errors = np.zeros(trainingSteps)
+# for step in range(trainingSteps):
+#     thetas -= learningRate * gradient(thetas)
+#     errors[step] = cost(thetas)
+
+# # J(theta) after gradient descent
+# print(cost(thetas))
+
+# # Plots Cost function as gradient descent runs
+# plt.plot(errors)
+# plt.xlabel('Training Steps')
+# plt.ylabel('Cost Function')
+plt.show()
