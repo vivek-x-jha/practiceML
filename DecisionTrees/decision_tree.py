f45ef39635e2f683b58fe0c79cc5bf102823f178
Vivek Jha <vivek.x.jha@gmail.com>
2018-04-14 00:58:13 -0700

    Edited print_tree func

--- DecisionTrees/decision_tree.py
+++ DecisionTrees/decision_tree.py
@@ -327,24 +327,24 @@ def build_tree(rows):
     return Decision_Node(question, true_branch, false_branch)
 
 
-def print_tree(node, spacing=""):
+def print_tree(node, spacing=''):
     """World's most elegant tree printing function."""
 
     # Base case: we've reached a leaf
     if isinstance(node, Leaf):
-        print (spacing + "Predict", node.predictions)
+        print(f'{spacing}Predict', node.predictions)
         return
 
     # Print the question at this node
-    print (spacing + str(node.question))
+    print(f'{spacing}{node.question}')
 
     # Call this function recursively on the true branch
-    print (spacing + '--> True:')
-    print_tree(node.true_branch, spacing + "  ")
+    print(f'{spacing}--> True:')
+    print_tree(node.true_branch, f'{spacing}  ')
 
     # Call this function recursively on the false branch
-    print (spacing + '--> False:')
-    print_tree(node.false_branch, spacing + "  ")
+    print(f'{spacing}--> False:')
+    print_tree(node.false_branch, f'{spacing}  ')
 
 
 def classify(row, node):
